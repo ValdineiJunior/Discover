e87abd44947191ac622ce93b7f620128e4a9bfad
Valdinei de Paula Junior <valdineidepaulajunior@gmail.com>
2022-05-12 00:52:32 -0300

    basic finish

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,6 +1,66 @@
+const Database = require('../db/config')
+
 module.exports = {
-  create(req, res) {
-    let roomId = 123456
+  async create(req, res) {
+    const db = await Database()
+    const pass = req.body.password
+    let roomId = 0
+    let isRoom = true
+    while (isRoom) {
+      roomId += Math.floor(Math.random() * 10)
+
+      for (var i = 0; i < 5; i++) {
+        roomId += Math.floor(Math.random() * 10).toString()
+      }
+      // Verificar se esse numero ja existe
+      const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+
+      isRoom = roomsExistIds.some(roomsExistId => roomsExistId === roomId)
+
+      if (!isRoom) {
+        // Inseri a sala no banco de dados
+        await db.run(`INSERT INTO rooms (
+          id,
+          pass
+        ) VALUES (
+          ${parseInt(roomId)},
+          "${pass}"
+        )`)
+      }
+    }
+
+    await db.close()
+
+    res.redirect(`/room/${roomId}`)
+  },
+  async open(req, res) {
+    const db = await Database()
+    const roomId = req.params.room
+    const questions = await db.all(
+      `SELECT * FROM questions WHERE room = ${roomId} and read = 0`
+    )
+    const questionsRead = await db.all(
+      `SELECT * FROM questions WHERE room = ${roomId} and read = 1`
+    )
+    let isNoQuestions
+
+    if (questions.length == 0) {
+      if (questionsRead.length == 0) {
+        isNoQuestions = true
+      }
+    }
+
+    res.render('room', {
+      roomId: roomId,
+      questions: questions,
+      questionsRead: questionsRead,
+      isNoQuestions: isNoQuestions
+    })
+  },
+
+  enter(req, res) {
+    const roomId = req.body.roomId
+
     res.redirect(`/room/${roomId}`)
   }
 }
--- src/route.js
+++ src/route.js
@@ -2,8 +2,6 @@ const express = require('express')
 const QuestionController = require('./controllers/QuestionController')
 const RoomController = require('./controllers/RoomController')
 
-const res = require('express/lib/response')
-
 const route = express.Router()
 
 route.get('/', (req, res) => res.render('index', { page: 'enter-room' }))
@@ -11,10 +9,11 @@ route.get('/create-pass', (req, res) =>
   res.render('index', { page: 'create-pass' })
 )
 
-route.get('/room/:room', (req, res) => res.render('room'))
-
-// Formato que o fomulario da modal pega as informações
-route.post('/question/:room/:question/:action', QuestionController.index)
 route.post('/create-room', RoomController.create)
+route.get('/room/:room', RoomController.open)
+
+route.post('/enterroom', RoomController.enter)
+route.post('/question/create/:room', QuestionController.create)
+route.post('/question/:room/:question/:action', QuestionController.index)
 
 module.exports = route
